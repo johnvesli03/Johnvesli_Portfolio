2c1b3678c377615f98e19640105f744e3709e2d4
John Vesli <Veslijohn3@gmail.com>
2025-07-04 13:02:08 +0530

    Add files via upload

---
+++ Portfolio-Website/assets/js/tawk.js
@@ -0,0 +1,13 @@
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6865701c5ad0e3190dc59e7f/1iv63rru9';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
